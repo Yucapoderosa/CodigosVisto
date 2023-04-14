99030f01769b60ee5f475765ecff18d767943c41
Yucapoderosa <luisdiegofc09@gmail.com>
2023-04-14 12:50:09 -0600

    LuisDiegoFloresLap7

--- Semana13/ProgramacionAsincrona/Main.py
+++ Semana13/ProgramacionAsincrona/Main.py
@@ -1,24 +1,33 @@
-#Tema asincronia
+# Tema asincronia
 import asyncio
+import random
 
 
 async def MetodoA():
     print('inicioA')
-    #conectarse a un BD y traer 5 millones
+    # conectarse a un BD y traer 5 millones
     await asyncio.sleep(30)
     print('finA')
-    
+
+
 async def MetodoB():
     print('inicioB')
-    #conecion a un servicio externo y esto tiene una duracion de 10
+    # conecion a un servicio externo y esto tiene una duracion de 20
     await asyncio.sleep(20)
     print('finB')
-    
-    
+
+
+async def ImprimirNumerosAleatorios():
+    for i in range(1000):
+        print(random.randint(1, 10))
+        await asyncio.sleep(0)
+
+
 async def main():
-    MetodoA() # 30
-    MetodoB() # 20
-    l = await asyncio.gather(MetodoA(),MetodoB()) #30
-    
-    
+    tareaA = MetodoA()  # 30
+    tareaB = MetodoB()  # 20
+    tareaC = ImprimirNumerosAleatorios()
+    await asyncio.gather(tareaA, tareaB, tareaC)
+
+
 asyncio.run(main())
